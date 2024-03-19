6102b577ab3cb982940e191d87f639f9d18223d1
Daweihao84 <dwsamurai84@gmail.com>
2024-03-19 23:57:48 +0800

    Add other colors in Colors.

--- HW#44-JSON Decoder/Colors.swift
+++ HW#44-JSON Decoder/Colors.swift
@@ -12,7 +12,14 @@ enum Colors {
     static let black:      UIColor = UIColor.black
     static let lightGray:  UIColor = UIColor.lightGray
     static let white:      UIColor = UIColor.white
-    static let brightGray: UIColor = UIColor.systemGray6
+
+    static let systemGray2: UIColor = UIColor.systemGray2
+    static let systemGray3: UIColor = UIColor.systemGray3
+    static let systemGray4: UIColor = UIColor.systemGray4
+    static let systemGray5: UIColor = UIColor.systemGray5
+    static let systemGray6: UIColor = UIColor.systemGray6
+    
     static let red:        UIColor = UIColor.red
+    static let systemYellow:     UIColor = UIColor.systemYellow
 }
 
