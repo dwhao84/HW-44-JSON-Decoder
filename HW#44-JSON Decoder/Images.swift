76aaa1c18d381e367a8fb6ffcc801731c561599e
Daweihao84 <dwsamurai84@gmail.com>
2024-03-19 23:59:31 +0800

    Add few elements to Images Constants.

--- HW#44-JSON Decoder/Images.swift
+++ HW#44-JSON Decoder/Images.swift
@@ -5,4 +5,12 @@
 //  Created by Dawei Hao on 2024/3/17.
 //
 
-import Foundation
+import UIKit
+
+struct Images {
+    static let mapping           = UIImage(systemName: "mappin.circle.fill")!
+    static let arrowTurnUpRight  = UIImage(systemName: "arrow.turn.up.right")!
+    static let starFill          = UIImage(systemName: "star.fill")!
+    static let informationCircle = UIImage(systemName: "info.circle.fill")!
+    static let locationFill      = UIImage(systemName: "location.fill")!
+}
