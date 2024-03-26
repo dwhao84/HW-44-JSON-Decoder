1d814de45b4686d0cf2c4a9ae3f36bb6cc7d67f8
Daweihao84 <dwsamurai84@gmail.com>
2024-03-27 00:43:41 +0800

    Create the dropShadow function for UIView.

--- HW#44-JSON Decoder/UIView+Ext.swift
+++ HW#44-JSON Decoder/UIView+Ext.swift
@@ -5,4 +5,14 @@
 //  Created by Dawei Hao on 2024/3/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func dropShadow () {
+        self.frame               = CGRect(x: 0, y: 0, width: 30, height: 30)
+        self.layer.shadowColor   = Colors.black.cgColor
+        self.layer.shadowOpacity = 0.4
+        self.layer.shadowOffset  = CGSize.zero
+        self.layer.shadowRadius  = 5
+    }
+}
