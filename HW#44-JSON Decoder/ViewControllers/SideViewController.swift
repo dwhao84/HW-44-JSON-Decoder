20a55eb64a05b58bea29c840df9d108124325b5d
Daweihao84 <dwsamurai84@gmail.com>
2024-04-05 00:33:49 +0800

    Create the sideVC for user, so that user could easily to switch pages.

--- HW#44-JSON Decoder/ViewControllers/SideViewController.swift
+++ HW#44-JSON Decoder/ViewControllers/SideViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class SideViewController: UIViewController {
-
+    
+    let favoriteBtn: UIButton = UIButton(type: .system)
+    let loginBtn: UIButton    = UIButton(type: .system)
+    let sideView: UIView      = UIView()
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = Colors.sideVCWithTransparent
+        
+        var config = UIButton.Configuration.plain()
+        var title = AttributedString("Favorite List")
+        title.foregroundColor = UIColor.darkGray
+        title.font = UIFont.boldSystemFont(ofSize: 20)
+        config.attributedTitle = title
+        config.automaticallyUpdateForSelection = true
+        config.titleAlignment = .center
+        favoriteBtn.configuration = config
+        
+        sideView.backgroundColor    = Colors.white
+        sideView.layer.cornerRadius = 20
+        sideView.clipsToBounds      = true
+        
+        view.addSubview(sideView)
+        sideView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            sideView.topAnchor.constraint(equalTo: view.topAnchor),
+            sideView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            sideView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            sideView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/5 )
+        ])
+        
+        sideView.addSubview(favoriteBtn)
+        favoriteBtn.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            favoriteBtn.topAnchor.constraint(equalTo: sideView.topAnchor, constant: 50),
+            favoriteBtn.centerXAnchor.constraint(equalTo: sideView.centerXAnchor)
+        ])
+        
     }
     
+    
 
-    /*
-    // MARK: - Navigation
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+#Preview {
+    UINavigationController(rootViewController: SideViewController())
 }
