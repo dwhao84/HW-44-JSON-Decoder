eb670519e5b681c7415df1659f3cc34f81a8df71
Daweihao84 <dwsamurai84@gmail.com>
2024-04-06 17:35:21 +0800

    Add personalView into the sideView, also set up the function tableView into the sideView.

--- HW#44-JSON Decoder/ViewControllers/SideViewController.swift
+++ HW#44-JSON Decoder/ViewControllers/SideViewController.swift
@@ -9,50 +9,87 @@ import UIKit
 
 class SideViewController: UIViewController {
     
-    let favoriteBtn: UIButton = UIButton(type: .system)
-    let loginBtn: UIButton    = UIButton(type: .system)
-    let sideView: UIView      = UIView()
- 
+    let personalView: PersonalView = PersonalView()
+    
+    let sideView: UIView = UIView()
+    let functionsTableView: UITableView = UITableView()
+    
+    let data: [String] = [
+        "登入資訊",
+        "最愛清單",
+    ]
+    
+    enum Constants {
+        static let imageViewWidth: CGFloat = 60.0
+        static let sideViewRadius: CGFloat = 20
+    }
+
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = Colors.sideVCWithTransparent
         
-        var config = UIButton.Configuration.plain()
-        var title = AttributedString("Favorite List")
-        title.foregroundColor = UIColor.darkGray
-        title.font = UIFont.boldSystemFont(ofSize: 20)
-        config.attributedTitle = title
-        config.automaticallyUpdateForSelection = true
-        config.titleAlignment = .center
-        favoriteBtn.configuration = config
+        setupUI()
         
-        sideView.backgroundColor    = Colors.white
-        sideView.layer.cornerRadius = 20
-        sideView.clipsToBounds      = true
+    }
+    
+    func setupUI () {
+        configureSideView()
+        constraintSideView()
+        delegateAndDataSource ()
+        constraintPersonalView ()
         
+    }
+    
+    func delegateAndDataSource () {
+        functionsTableView.delegate = self
+        functionsTableView.dataSource = self
+    }
+    
+    func constraintPersonalView () {
+        sideView.addSubview(personalView)
+        personalView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            personalView.topAnchor.constraint(equalTo: sideView.topAnchor, constant: 30),
+            personalView.leadingAnchor.constraint(equalTo: sideView.leadingAnchor),
+            personalView.trailingAnchor.constraint(equalTo: sideView.trailingAnchor),
+            personalView.heightAnchor.constraint(equalTo: sideView.widthAnchor, multiplier: 0.3)
+        ])
+    }
+    
+    func constraintSideView() {
         view.addSubview(sideView)
         sideView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             sideView.topAnchor.constraint(equalTo: view.topAnchor),
             sideView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             sideView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            sideView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/5 )
+            sideView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 3/5 )
         ])
-        
-        sideView.addSubview(favoriteBtn)
-        favoriteBtn.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            favoriteBtn.topAnchor.constraint(equalTo: sideView.topAnchor, constant: 50),
-            favoriteBtn.centerXAnchor.constraint(equalTo: sideView.centerXAnchor)
-        ])
-        
+    }
+    
+    func configureSideView() {
+        sideView.backgroundColor    = Colors.white
+        sideView.layer.cornerRadius = Constants.sideViewRadius
+        sideView.clipsToBounds      = true
+    }
+}
+
+extension SideViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
     }
     
     
-
-    
 }
+
+
 
 #Preview {
     UINavigationController(rootViewController: SideViewController())
