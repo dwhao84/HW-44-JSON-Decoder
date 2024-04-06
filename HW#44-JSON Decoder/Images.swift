7eefc8b8e6a1679a97d364a8934c26d42c2a4d33
Daweihao84 <dwsamurai84@gmail.com>
2024-04-06 21:31:09 +0800

    Add data for function UIImage.

--- HW#44-JSON Decoder/Images.swift
+++ HW#44-JSON Decoder/Images.swift
@@ -18,5 +18,14 @@ struct Images {
     static let listBullet        = UIImage(systemName: "list.bullet")!
     static let listStar          = UIImage(systemName: "list.star")!
     static let radioWave         = UIImage(systemName: "dot.radiowaves.up.forward")!
-    static let person            = UIImage(systemName: "person.circle")!
+    static let person            = UIImage(systemName: "person.circle.fill")!
+    
+    
+    static let message = UIImage(systemName: "ellipsis.message")!
+    static let network = UIImage(systemName: "network")!
+    static let house = UIImage(systemName: "house.fill")!
+    static let doc = UIImage(systemName: "doc.text")!
+    static let badget = UIImage(systemName: "shield.lefthalf.filled.badge.checkmark")!
+    static let lostAndFound = UIImage(systemName: "bag.badge.questionmark")!
+    static let radioWavesSec = UIImage(systemName: "antenna.radiowaves.left.and.right.circle")!
 }
