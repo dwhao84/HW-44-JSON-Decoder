a86c4367f79b598889095250e168a6b581965511
Daweihao84 <dwsamurai84@gmail.com>
2024-04-06 17:30:01 +0800

    Create the functionTableViewCell.

--- HW#44-JSON Decoder/CustomTableViewCell/FunctionsTableViewCell.swift
+++ HW#44-JSON Decoder/CustomTableViewCell/FunctionsTableViewCell.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class FunctionsTableViewCell: UITableViewCell {
+    
+    static let identifier: String = "FunctionsTableViewCell"
 
+    let functionsTitle: UILabel = UILabel()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configureFunctionLabel()
+        constraintFunctionLabel()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +25,31 @@ class FunctionsTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        print("prepareForReuse")
+    }
+    
+    func configureFunctionLabel () {
+        functionsTitle.text = "XXX"
+        functionsTitle.textColor = Colors.darkGray
+        functionsTitle.adjustsFontSizeToFitWidth = true
+        functionsTitle.textAlignment = .center
+        functionsTitle.font = UIFont.systemFont(ofSize: 20)
+    }
+    
+    func constraintFunctionLabel () {
+        self.addSubview(functionsTitle)
+        functionsTitle.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            functionsTitle.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            functionsTitle.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+    }
 }
+
+#Preview(traits: .fixedLayout(width: 100, height: 20), body: {
+    let cell = FunctionsTableViewCell()
+    return cell
+})
