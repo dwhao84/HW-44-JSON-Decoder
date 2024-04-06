87c153067f2efd63789e5af9b54b76b5e96e3fc3
Daweihao84 <dwsamurai84@gmail.com>
2024-04-06 21:28:06 +0800

    Create the functions data.

--- HW#44-JSON Decoder/Models/FunctionModel.swift
+++ HW#44-JSON Decoder/Models/FunctionModel.swift
@@ -5,4 +5,21 @@
 //  Created by Dawei Hao on 2024/4/6.
 //
 
-import Foundation
+import UIKit
+
+struct Functions {
+    let serviceImage: UIImage
+    let serviceTitle: String
+}
+
+let servicesArray: [Functions] = [
+    Functions(serviceImage: Images.bike, serviceTitle: "Station Map"),
+    Functions(serviceImage: Images.locationFill, serviceTitle: "Change Location"),
+    Functions(serviceImage: Images.message, serviceTitle: "News Updates"),
+    Functions(serviceImage: Images.network, serviceTitle: "Activity"),
+    Functions(serviceImage: Images.house, serviceTitle: "Service Center"),
+    Functions(serviceImage: Images.doc, serviceTitle: "Rental Instructions"),
+    Functions(serviceImage: Images.badget, serviceTitle: "Get Insured"),
+    Functions(serviceImage: Images.lostAndFound, serviceTitle: "Lost & Found"),
+    Functions(serviceImage: Images.radioWavesSec, serviceTitle: "Report Lost YouBike"),
+]
