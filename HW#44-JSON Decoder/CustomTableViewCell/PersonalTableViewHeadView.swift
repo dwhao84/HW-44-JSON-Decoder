999fa1ebfbb71aa3f860ebb670535e1908ec1bf7
Daweihao84 <dwsamurai84@gmail.com>
2024-04-06 21:26:32 +0800

    Change the file path.

---
+++ HW#44-JSON Decoder/CustomTableViewCell/PersonalTableViewHeadView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalTableViewHeadView.swift
+//  HW#44-JSON Decoder
+//
+//  Created by Dawei Hao on 2024/4/6.
+//
+
+import UIKit
+
+class PersonalTableViewHeadView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HW#44-JSON Decoder/Models/FunctionModel.swift
@@ -0,0 +1,8 @@
+//
+//  FunctionModel.swift
+//  HW#44-JSON Decoder
+//
+//  Created by Dawei Hao on 2024/4/6.
+//
+
+import Foundation
