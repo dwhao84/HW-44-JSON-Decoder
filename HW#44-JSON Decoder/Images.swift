38a34b836ee2e92a989ae2575b5a19b3a8dad9ff
Daweihao84 <dwsamurai84@gmail.com>
2024-04-07 00:52:07 +0800

    Add type for images.

--- HW#44-JSON Decoder/Images.swift
+++ HW#44-JSON Decoder/Images.swift
@@ -8,24 +8,24 @@
 import UIKit
 
 struct Images {
-    static let mapping           = UIImage(systemName: "mappin.circle.fill")!
-    static let arrowTurnUpRight  = UIImage(systemName: "arrow.turn.up.right")!
-    static let starFill          = UIImage(systemName: "star.fill")!
-    static let informationCircle = UIImage(systemName: "info.circle.fill")!
-    static let locationFill      = UIImage(systemName: "location.fill")!
-    static let bike              = UIImage(systemName: "bicycle")!
-    static let parkingSign       = UIImage(systemName: "parkingsign.circle")!
-    static let listBullet        = UIImage(systemName: "list.bullet")!
-    static let listStar          = UIImage(systemName: "list.star")!
-    static let radioWave         = UIImage(systemName: "dot.radiowaves.up.forward")!
-    static let person            = UIImage(systemName: "person.circle.fill")!
+    static let mapping           : UIImage = UIImage(systemName: "mappin.circle.fill")!
+    static let arrowTurnUpRight  : UIImage = UIImage(systemName: "arrow.turn.up.right")!
+    static let starFill          : UIImage = UIImage(systemName: "star.fill")!
+    static let informationCircle : UIImage = UIImage(systemName: "info.circle.fill")!
+    static let locationFill      : UIImage = UIImage(systemName: "location.fill")!
+    static let bike              : UIImage = UIImage(systemName: "bicycle")!
+    static let parkingSign       : UIImage = UIImage(systemName: "parkingsign.circle")!
+    static let listBullet        : UIImage = UIImage(systemName: "list.bullet")!
+    static let listStar          : UIImage = UIImage(systemName: "list.star")!
+    static let radioWave         : UIImage = UIImage(systemName: "dot.radiowaves.up.forward")!
+    static let person            : UIImage = UIImage(systemName: "person.circle.fill")!
     
     
-    static let message = UIImage(systemName: "ellipsis.message")!
-    static let network = UIImage(systemName: "network")!
-    static let house = UIImage(systemName: "house.fill")!
-    static let doc = UIImage(systemName: "doc.text")!
-    static let badget = UIImage(systemName: "shield.lefthalf.filled.badge.checkmark")!
-    static let lostAndFound = UIImage(systemName: "bag.badge.questionmark")!
-    static let radioWavesSec = UIImage(systemName: "antenna.radiowaves.left.and.right.circle")!
+    static let message       : UIImage = UIImage(systemName: "ellipsis.message")!
+    static let network       : UIImage = UIImage(systemName: "network")!
+    static let house         : UIImage = UIImage(systemName: "house.fill")!
+    static let doc           : UIImage = UIImage(systemName: "doc.text")!
+    static let badget        : UIImage = UIImage(systemName: "shield.lefthalf.filled.badge.checkmark")!
+    static let lostAndFound  : UIImage = UIImage(systemName: "bag.badge.questionmark")!
+    static let radioWavesSec : UIImage = UIImage(systemName: "antenna.radiowaves.left.and.right.circle")!
 }
