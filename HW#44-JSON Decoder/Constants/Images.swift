147beb02dbe7360ea586c8c76e1068253151bd76
Daweihao84 <dwsamurai84@gmail.com>
2024-04-08 00:58:40 +0800

    Add the custom tableViewCell, and import the core data function.

---
+++ HW#44-JSON Decoder/Constants/Images.swift
@@ -0,0 +1,37 @@
+//
+//  Images.swift
+//  HW#44-JSON Decoder
+//
+//  Created by Dawei Hao on 2024/3/17.
+//
+
+import UIKit
+
+struct Images {
+    static let mapping           : UIImage    = UIImage(systemName: "mappin.circle.fill")!
+    static let arrowTurnUpRight  : UIImage    = UIImage(systemName: "arrow.turn.up.right")!
+    
+    static let starFill          : UIImage    = UIImage(systemName: "star.fill")!
+    static let star              : UIImage    = UIImage(systemName: "star")!
+    
+    static let informationCircle : UIImage    = UIImage(systemName: "info.circle.fill")!
+    static let locationFill      : UIImage    = UIImage(systemName: "location.fill")!
+    static let bike              : UIImage    = UIImage(systemName: "bicycle")!
+    static let parkingSign       : UIImage    = UIImage(systemName: "parkingsign.circle")!
+    static let listBullet        : UIImage    = UIImage(systemName: "list.bullet")!
+    static let listStar          : UIImage    = UIImage(systemName: "list.star")!
+    static let radioWave         : UIImage    = UIImage(systemName: "dot.radiowaves.up.forward")!
+    static let person            : UIImage    = UIImage(systemName: "person.circle.fill")!
+       
+    static let message       : UIImage        = UIImage(systemName: "ellipsis.message")!
+    static let network       : UIImage        = UIImage(systemName: "network")!
+    static let house         : UIImage        = UIImage(systemName: "house.fill")!
+    static let doc           : UIImage        = UIImage(systemName: "doc.text")!
+    static let badget        : UIImage        = UIImage(systemName: "shield.lefthalf.filled.badge.checkmark")!
+    static let lostAndFound  : UIImage        = UIImage(systemName: "bag.badge.questionmark")!
+    static let radioWavesSec : UIImage        = UIImage(systemName: "antenna.radiowaves.left.and.right.circle")!
+    
+    static let bikeWithCycle : UIImage        = UIImage(systemName: "bicycle.circle")!
+    static let parkingsignWithCycle : UIImage = UIImage(systemName: "parkingsign.circle")!
+    
+}
--- HW#44-JSON Decoder/ViewControllers/FavoriteListViewController.swift
+++ HW#44-JSON Decoder/ViewControllers/FavoriteListViewController.swift
@@ -6,20 +6,23 @@
 //
 
 import UIKit
+import CoreData
 
 class FavoriteListViewController: UIViewController {
     
     let favoriteListTableView: UITableView = UITableView()
+    let refreshControl: UIRefreshControl = UIRefreshControl()
     
-    var dataArray: [String] = [""]
+    // Save context by using Core data.
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    private var modals = [FavoriteListData]()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI ()
-        
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -30,13 +33,28 @@ class FavoriteListViewController: UIViewController {
     func setupUI () {
         configureFavoriteListTableView ()
         constraintsTableView()
+        setupRefreshControl()
     }
     
+    // MARK: - Refresh Control
+    func setupRefreshControl () {
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: #selector(self.refresh), for: .valueChanged)
+        favoriteListTableView.addSubview(refreshControl)
+    }
+    
+    @objc func refresh(_ sender: AnyObject) {
+        // Code to refresh table view
+        refreshControl.endRefreshing()
+        favoriteListTableView.reloadData()
+    }
+    
+    // MARK: - Favorite List TableView
     func configureFavoriteListTableView () {
         favoriteListTableView.dataSource = self
         favoriteListTableView.delegate   = self
-        favoriteListTableView.rowHeight  = 120
-        
+        favoriteListTableView.rowHeight  = 100
+        favoriteListTableView.register(FavoriteListTableViewCell.self, forCellReuseIdentifier: FavoriteListTableViewCell.identifier)
         self.navigationItem.title = "Favorite List"
     }
     
@@ -51,16 +69,64 @@ class FavoriteListViewController: UIViewController {
         ])
     }
     
+    // MARK: - Core data
+    
+    func getAllItems () {
+        do {
+            modals = try context.fetch(FavoriteListData.fetchRequest())
+            DispatchQueue.main.async {
+                self.favoriteListTableView.reloadData()
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    func createItem(address: String, bikeQty: String, dockQty: String) {
+        let newItem = FavoriteListData(context: context)
+        newItem.address = address
+        newItem.bikeQty = bikeQty
+        newItem.dockQty = dockQty
+        
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func deleteItem(item: FavoriteListData) {
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func updateItem(item: FavoriteListData, newName: String) {
+        item.stationName = String()
+        item.dockQty = String()
+        item.bikeQty = String()
+        item.address = String()
+        
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
 }
 
+// MARK: - Extension:
 extension FavoriteListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         1
+        1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        UITableViewCell ()
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteListTableViewCell.identifier, for: indexPath)
+//        let model = modals[indexPath.row]
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -71,7 +137,7 @@ extension FavoriteListViewController: UITableViewDataSource, UITableViewDelegate
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         let deleteAction = UIContextualAction(style: .normal, title: "刪除") { (action, view, completionHandler) in
-            print("一點點不動心")
+            print("刪除內容")
             completionHandler(false)
         }
         
