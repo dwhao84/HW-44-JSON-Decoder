b1e893dc1d2955db5a846e7ed13f6156edcfe332
Daweihao84 <dwsamurai84@gmail.com>
2024-04-11 18:52:56 +0800

    Changed the data model's name.

--- HW#44-JSON Decoder/Supporting Files/AppDelegate.swift
+++ HW#44-JSON Decoder/Supporting Files/AppDelegate.swift
@@ -45,7 +45,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          error conditions that could cause the creation of the store to fail.
          */
         
-        let container = NSPersistentContainer(name: "FavoriteListData")
+        let container = NSPersistentContainer(name: "FavoriteListDataModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
