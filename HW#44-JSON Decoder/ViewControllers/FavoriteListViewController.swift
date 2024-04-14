6fd7457df68e82297fcf18be49e9f98dfb07166b
Daweihao84 <dwsamurai84@gmail.com>
2024-04-15 00:42:40 +0800

    Add fetchedResultsController into code to manage the core data and able to display on the tableView.

--- HW#44-JSON Decoder/ViewControllers/FavoriteListViewController.swift
+++ HW#44-JSON Decoder/ViewControllers/FavoriteListViewController.swift
@@ -1,112 +1,80 @@
-//
-//  FavoriteListViewController.swift
-//  HW#44-JSON Decoder
-//
-//  Created by Dawei Hao on 2024/4/5.
-//
-
 import UIKit
 import CoreData
 
-class FavoriteListViewController: UIViewController, NSFetchedResultsControllerDelegate {
+class FavoriteListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
     
     var fetchedResultsController: NSFetchedResultsController<FavoriteListData>!
     
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "FavoriteListDataModel")
-        container.loadPersistentStores { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        }
-        return container
-    }()
-    
-    var context: NSManagedObjectContext {
-        return persistentContainer.viewContext
-    }
+    var coreData: CoreDataStack = CoreDataStack()
     
     var managedObjectContext: NSManagedObjectContext!
     
     let favoriteListTableView: UITableView = UITableView()
     let refreshControl: UIRefreshControl = UIRefreshControl()
     
-    // Save context by using Core data.
-    let request: NSFetchRequest<FavoriteListData> = FavoriteListData.fetchRequest()
-    
-    private var modals = [FavoriteListData]()
-    
-    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("DEBUG PRINT: FavTableVC viewDidLoad")
+        managedObjectContext = coreData.persistentContainer.viewContext
         
-        setupUI ()
+        setupUI()
+        initializeFetchedResultsController()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        print("DEBUG PRINT: didReceiveMemoryWarning")
-    }
-    
-    func loadFavoritePlaces () {
+    func initializeFetchedResultsController() {
+        let request: NSFetchRequest<FavoriteListData> = FavoriteListData.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "stationName", ascending: true)
+        request.sortDescriptors = [sortDescriptor]
+
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+
         do {
-            modals = try context.fetch(request)
-            favoriteListTableView.reloadData()
-            print("DEBUG PRINT: 資料數\(modals.count)")
+            try fetchedResultsController.performFetch()
         } catch {
-            print("DEBUG PRINT: Unable to fetch data.")
+            print("Failed to initialize FetchedResultsController: \(error)")
         }
-        print(modals)
     }
-    
-    func setupUI () {
-        configureFavoriteListTableView ()
-        constraintsTableView()
+
+    func setupUI() {
+        configureFavoriteListTableView()
+        configureNavigationBar()
         setupRefreshControl()
-        loadFavoritePlaces()
-        configureNC()
-        
     }
-    
-    func configureNC () {
+
+    func configureNavigationBar() {
         let appearance = UINavigationBarAppearance()
-        self.navigationController?.navigationBar.isTranslucent = true
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: Colors.darkGray]
-        
-        self.navigationController?.navigationBar.standardAppearance = appearance
+        appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = Colors.lightGray
-        self.navigationController?.navigationBar.tintColor = Colors.systemYellow
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: Colors.darkGray]
         
-        self.navigationItem.title = "Favorite Station List"
-        self.view.backgroundColor = Colors.white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.tintColor = Colors.systemYellow
+        
+        navigationItem.title = "Favorite Station List"
+        view.backgroundColor = Colors.white
     }
-    
-    // MARK: - Refresh Control
-    func setupRefreshControl () {
+
+    func setupRefreshControl() {
         refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
-        refreshControl.addTarget(self, action: #selector(self.refresh), for: .valueChanged)
+        refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
         favoriteListTableView.addSubview(refreshControl)
     }
-    
+
     @objc func refresh(_ sender: AnyObject) {
         // Code to refresh table view
         refreshControl.endRefreshing()
         favoriteListTableView.reloadData()
     }
-    
-    // MARK: - Favorite List TableView
-    func configureFavoriteListTableView () {
+
+    func configureFavoriteListTableView() {
         favoriteListTableView.backgroundColor = Colors.white
         favoriteListTableView.dataSource = self
-        favoriteListTableView.delegate   = self
-        favoriteListTableView.rowHeight  = 100
+        favoriteListTableView.delegate = self
+        favoriteListTableView.rowHeight = 100
         favoriteListTableView.register(FavoriteListTableViewCell.nib(), forCellReuseIdentifier: FavoriteListTableViewCell.identifier)
         favoriteListTableView.frame = view.bounds
-    }
-    
-    func constraintsTableView () {
         view.addSubview(favoriteListTableView)
+        
         favoriteListTableView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             favoriteListTableView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -115,82 +83,61 @@ class FavoriteListViewController: UIViewController, NSFetchedResultsControllerDe
             favoriteListTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
-}
 
-// MARK: - Extension:
-extension FavoriteListViewController: UITableViewDataSource, UITableViewDelegate {
+    // UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // Show the data which saved in modals by using Core data.
-        modals.count - 1
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteListTableViewCell.identifier, for: indexPath) as? FavoriteListTableViewCell else {
-            fatalError("DEBUG PRINT: Could not dequeue cell with identifier: \(FavoriteListTableViewCell.identifier)")
-        }
-        
-        tableView.allowsSelection = true
-        
-        let place = modals[indexPath.row]
-        cell.stationNameLabel.text = place.stationName
-        cell.addressLabel.text = place.address
-        cell.bikeQtyLabel.text = place.bikeQty
-        cell.dockQtyLabel.text = place.dockQty
-        
-        print(place.address ?? "Can't find address.",
-              place.stationName ?? "Can't find stationName",
-              place.bikeQty ?? "Can't find bikeQty.",
-              place.dockQty ?? "Can't find dockQty."
-        )
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoriteListTableViewCell.identifier, for: indexPath) as! FavoriteListTableViewCell
+        configureCell(cell, at: indexPath)
+        cell.backgroundColor = Colors.white
         return cell
     }
+
+    func configureCell(_ cell: FavoriteListTableViewCell, at indexPath: IndexPath) {
+        let favoriteListData = fetchedResultsController.object(at: indexPath)
+        // 使用安全的可选链或提供默认值来避免崩溃
+        cell.stationNameLabel?.text = favoriteListData.stationName ?? "Unknown Station"
+        cell.addressLabel?.text = favoriteListData.address ?? "No Address"
+        cell.bikeQtyLabel?.text = favoriteListData.bikeQty ?? "N/A"
+        cell.dockQtyLabel?.text = favoriteListData.dockQty ?? "N/A"
+    }
     
+    // UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        print(indexPath.row)
     }
-    
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let deleteAction = UIContextualAction(style: .destructive, title: "刪除") { [weak self] (action, view, completionHandler) in
-            guard let self = self else { return }
-            
-            print("DEBUG PRINT: delete action tapped")
-            
-            // Performing batch delete operation
-            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "FavoriteListData")
-            let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
-            do {
-                try self.context.execute(batchDeleteRequest)
-                // Consider merging the following save with the above operation if not needed
-                self.context.reset()
-            } catch {
-                print("Error \(error)")
-            }
 
-            // Re-fetch and update UI
-            do {
-                try self.fetchedResultsController?.performFetch()
-                DispatchQueue.main.async {
-                    // Reload the data and update the UI
-                    self.favoriteListTableView.reloadData()
-                }
-            } catch {
-                print("Failed to fetch entities after deletion: \(error)")
-            }
+    // Swipe to delete
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { action, view, completionHandler in
+            self.deleteFavorite(at: indexPath)
             completionHandler(true)
         }
-        
         deleteAction.backgroundColor = Colors.red
-        
-        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
-        configuration.performsFirstActionWithFullSwipe = true
-
-        return configuration
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
-}
 
+    func deleteFavorite(at indexPath: IndexPath) {
+        let favoriteToDelete = fetchedResultsController.object(at: indexPath)
+        managedObjectContext.delete(favoriteToDelete)
+        do {
+            try managedObjectContext.save()
+        } catch {
+            print("Error saving context after deletion: \(error)")
+        }
+    }
 
-
-#Preview {
-    UINavigationController(rootViewController: FavoriteListViewController())
+    // NSFetchedResultsControllerDelegate
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        DispatchQueue.main.async {
+            self.favoriteListTableView.reloadData()
+        }
+    }
 }
