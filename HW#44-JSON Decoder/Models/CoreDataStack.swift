f34f7615bd8ba357119f0dcfba6ea87652409be0
Daweihao84 <dwsamurai84@gmail.com>
2024-04-15 00:02:27 +0800

    Add CoreDataStack to handling the data.

--- HW#44-JSON Decoder/Models/CoreDataStack.swift
+++ HW#44-JSON Decoder/Models/CoreDataStack.swift
@@ -5,4 +5,31 @@
 //  Created by Dawei Hao on 2024/4/13.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class CoreDataStack {
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "FavoriteListDataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
